fda49382d55ab4b3db999ce2988828f88eae5c25
Samuel Ivarsson <isamuel@student.chalmers.se>
2020-02-12 20:59:37 +0100

    Initial Commit

---
+++ spec/iv-profiles-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import IvProfilesView from '../lib/iv-profiles-view';
+
+describe('IvProfilesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
